02bb77af2760a816118fffd360aeb9e47baf867d
kky04994 <kkyo8747@naver.com>
2021-09-01 19:52:47 +0900

    ⚡️ :: (#25) 회원가입 컨테이너 추가

--- zamong/src/components/Signup/2/Signup.tsx
+++ zamong/src/components/Signup/2/Signup.tsx
@@ -1,15 +1,15 @@
 import * as S from "./Style";
 
-const Login = (): JSX.Element => {
+const Signup2 = (): JSX.Element => {
   return (
     <S.SignupBox>
       <S.Title>회원가입</S.Title>
       <S.GuideWord>이메일</S.GuideWord>
       <S.Input />
       <S.GuideWord>비밀번호</S.GuideWord>
-      <S.Input type="password"/>
+      <S.Input type="password" />
       <S.GuideWord>비밀번호 확인</S.GuideWord>
-      <S.Check type="password"/>
+      <S.Check type="password" />
       <S.EventBox>
         <S.PrevButton>이전</S.PrevButton>
         <S.NextButton>다음</S.NextButton>
@@ -18,4 +18,4 @@ const Login = (): JSX.Element => {
   );
 };
 
-export default Login;
+export default Signup2;
--- zamong/src/container/index.ts
+++ zamong/src/container/index.ts
@@ -1,1 +1,2 @@
 export { default as LoginContainer } from "./Login";
+export { default as SignupContainer } from "./Signup";
