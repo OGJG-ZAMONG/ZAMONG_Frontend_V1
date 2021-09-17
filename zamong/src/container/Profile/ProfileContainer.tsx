f16f77b89b6743cbd902be3673c55e225dd1c166
kky04994 <kkyo8747@naver.com>
2021-09-17 23:46:33 +0900

    ⚡️ :: (#20) ProfileContainer 생성

---
+++ zamong/src/container/Profile/ProfileContainer.tsx
@@ -0,0 +1,12 @@
+import React, { Suspense, FC } from "react";
+import { ProfilePage } from "../../components/Profile/ProfilePage";
+
+const ProfileContainter: FC = (): JSX.Element => {
+  return (
+    <Suspense fallback={<div>로딩중...</div>}>
+      <ProfilePage />
+    </Suspense>
+  );
+};
+
+export default ProfileContainter;
