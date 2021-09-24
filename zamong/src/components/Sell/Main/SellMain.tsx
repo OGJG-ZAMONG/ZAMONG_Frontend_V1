fd432668579995b9404a36e353ecb3db6d6ba5e9
Jun Joy <chojuneseo1@gmail.com>
2021-09-24 23:28:23 +0900

    ⚡️ :: (#43) Sell Main UI

--- zamong/src/components/Sell/Main/SellMain.tsx
+++ zamong/src/components/Sell/Main/SellMain.tsx
@@ -1,9 +1,10 @@
 import * as S from "./style";
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import FollowDreamDiary from "../../CardDream/FollowDreamDiary/FollowDreamDiary";
 
 const SellMain:FC = ():JSX.Element => {
     const pageLength = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10];
+    
     return (
         <S.Container>
             <S.Information>
