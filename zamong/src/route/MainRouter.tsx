e9807871732d01cf11efa16965aedbba1a02151e
kky04994 <kkyo8747@naver.com>
2021-09-27 01:15:49 +0900

    :memo: :: (#20) 재정렬

--- zamong/src/route/MainRouter.tsx
+++ zamong/src/route/MainRouter.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { Switch, Route } from "react-router-dom";
 import { LoginContainer, DiaryContainer, SignupContainer } from "../container";
-const MainRouter:FC = (): JSX.Element => {
+const MainRouter: FC = (): JSX.Element => {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={MainContainer}/>
+        <Route exact path="/" component={MainContainer} />
         <Route exact path="/login" component={LoginContainer} />
         <Route exact path="/diary" component={DiaryContainer} />
         <Route exact path="/signup" component={SignupContainer} />
+        {/* <Route exact path="/profile" component={} /> */}
       </Switch>
     </>
   );
