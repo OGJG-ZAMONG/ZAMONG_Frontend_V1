f0e547c698f32999d8fe7354c3212f37ccb9324e
kky04994 <kkyo8747@naver.com>
2021-09-29 22:39:50 +0900

    :bookmark_tabs: :: (#33) sellDreamContainer 경로 수정

---
+++ zamong/src/container/Sell/Main/SellDreamContainer.tsx
@@ -0,0 +1,15 @@
+import SellMain from "../../../components/Sell/Main/SellMain";
+import Header from "../../../components/Header/Header";
+import Footer from "../../../components/Footer/Footer";
+
+const SellDreamContainer = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <SellMain />
+      <Footer />
+    </>
+  );
+};
+
+export default SellDreamContainer;
--- zamong/src/container/index.ts
+++ zamong/src/container/index.ts
@@ -2,5 +2,5 @@ export { default as LoginContainer } from "./Login";
 export { default as MainContainer } from "./MainPage";
 export { default as DiaryContainer } from "./Diary";
 export { default as SignupContainer } from "./Signup";
-export { default as SellDreamContainer } from "./Sell";
+export { default as SellDreamContainer } from "./Sell/Main";
 export { default as DiaryWriteContainer } from "./DiaryWrite";
