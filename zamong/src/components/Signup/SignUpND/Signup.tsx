61ec56302335ed726a396a5ac60d1a49d5eb6ffd
wotjd1799 <wotjd0676@gmail.com>
2021-09-30 08:54:26 +0900

    ⚡️ :: (#5) Modify UI
    회원가입 2,3번째 컴포넌트 수정 및 파일 이름 변경

--- zamong/src/components/Signup/SignUpND/Signup.tsx
+++ zamong/src/components/Signup/SignUpND/Signup.tsx
@@ -1,13 +1,15 @@
-import * as S from "./Style";
+import * as S from "./styles";
 
 const Login = (): JSX.Element => {
   return (
     <S.SignupBox>
       <S.Title>회원가입</S.Title>
-      <S.GuideWord>아이디</S.GuideWord>
-      <S.IdInput />
-      <S.GuideWord>이름</S.GuideWord>
+      <S.GuideWord>이메일</S.GuideWord>
       <S.Input />
+      <S.GuideWord>비밀번호</S.GuideWord>
+      <S.Input type="password"/>
+      <S.GuideWord>비밀번호 확인</S.GuideWord>
+      <S.Check type="password"/>
       <S.EventBox>
         <S.PrevButton>이전</S.PrevButton>
         <S.NextButton>다음</S.NextButton>
--- zamong/src/components/Signup/SignUpRD/Signup.tsx
+++ zamong/src/components/Signup/SignUpRD/Signup.tsx
@@ -1,4 +1,4 @@
-import * as S from "./Style";
+import * as S from "./styles";
 import { MutableRefObject, useRef } from "react"
 
 const Login = (): JSX.Element => {
