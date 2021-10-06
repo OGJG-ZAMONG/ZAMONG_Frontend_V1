55dbb1c7350df4d70e21f8649cf46dfb4869e363
wotjd1799 <wotjd0676@gmail.com>
2021-10-06 15:38:31 +0900

    :bug: :: (#54) Change Container name
    컨테이너 이름 변경 및 폴더이름 변경

--- zamong/src/container/index.ts
+++ zamong/src/container/index.ts
@@ -1,6 +1,6 @@
 export { default as LoginContainer } from "./Login";
 export { default as MainContainer } from "./MainPage";
 export { default as DiaryContainer } from "./Diary";
-export { default as SignupContainer } from "./SignUp";
+export { default as SignUpContainer } from "./SignUp";
 export { default as SellDreamContainer } from "./Sell";
 export { default as DiaryWriteContainer } from "./DiaryWrite";
