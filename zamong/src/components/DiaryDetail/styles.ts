e1d539e3b44e5381a32bab7ff07948df38e73d94
wotjd1799 <wotjd0676@gmail.com>
2021-10-06 19:08:48 +0900

    ⚡️ :: (#32) Add Header
    디자인실수로 인해  없던 헤더 추가

--- zamong/src/components/DiaryDetail/styles.ts
+++ zamong/src/components/DiaryDetail/styles.ts
@@ -3,4 +3,5 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
   width: 1280px;
   margin: 0 auto;
+  padding-top: 64xpx;
 `;
--- zamong/src/container/DiaryDetail/index.tsx
+++ zamong/src/container/DiaryDetail/index.tsx
@@ -1,9 +1,11 @@
+import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import DiaryDetail from "../../components/DiaryDetail/DiaryDetail";
 
 const DiaryDetailContainer = ():JSX.Element => {
     return (
       <div>
+        <Header />
         <DiaryDetail />
         <Footer />
       </div>
