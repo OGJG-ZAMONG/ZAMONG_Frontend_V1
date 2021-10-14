85213ce6854330c521883c03a649c6e08caba1a7
김진근 <freedom7113@gmail.com>
2021-10-14 19:25:17 +0900

    ⚡️ :: (#72) sell write ui
    꿈 판매글 작성 퍼블리싱 완료

---
+++ zamong/src/container/SellWrite/index.tsx
@@ -0,0 +1,15 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import SellWrite from "../../components/SellWrite/SellWrite";
+
+const SellWriteContainer = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+      <SellWrite />
+      <Footer />
+    </>
+  );
+};
+
+export default SellWriteContainer;
