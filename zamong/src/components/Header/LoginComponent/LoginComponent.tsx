eafb746a18d041f3561274560e9fe62ca93aa05a
김진근 <freedom7113@gmail.com>
2021-10-14 10:34:17 +0900

    ⚡️ :: (#63) add nav links
    로그인 시 헤더를 추가했다

---
+++ zamong/src/components/Header/LoginComponent/LoginComponent.tsx
@@ -0,0 +1,12 @@
+import * as S from "../styles";
+
+const LoginComponent = (): JSX.Element => {
+  return (
+    <>
+      <S.BlueButton>로그인</S.BlueButton>
+      <S.BorderButton>회원가입</S.BorderButton>
+    </>
+  );
+};
+
+export default LoginComponent;
