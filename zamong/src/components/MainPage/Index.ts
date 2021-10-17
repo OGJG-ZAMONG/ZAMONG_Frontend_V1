f118f7a9b8a92ac64a983d56db5219cbdca31f56
kky04994 <kkyo8747@naver.com>
2021-10-17 15:19:27 +0900

    ⚡️ :: (#20) style에서 index로 수정

--- zamong/src/components/MainPage/Index.ts
+++ zamong/src/components/MainPage/Index.ts
@@ -1,3 +1,7 @@
-import styled from "@emotion/styled";
-import { color } from "../../style/color";
-import { font } from "../../style/font";
+export { default as FollowList } from "./FollowList/FollowList";
+export { default as DreamList } from "./DreamList/DreamList";
+export { default as Footer } from "../Footer/Footer";
+export { default as Dream } from "../Dream/Dream";
+export { default as MyDreamDiaryList } from "./DreamDiaryList/MyDreamDiaryList/MyDreamDiaryList";
+export { default as FollowDreamDiaryList } from "./DreamDiaryList/FollowDreamDiaryList/FollowDreamDiaryList";
+export { default as MyDreamDiary } from "../CardDream/MyDreamDiary/MyDreamDiary";
