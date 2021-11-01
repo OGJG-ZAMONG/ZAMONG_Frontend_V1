76fa600359d59a908cc0bad1a017b670e27dba79
김진근 <freedom7113@gmail.com>
2021-11-01 22:19:36 +0900

    :recycle: :: (#127) add response type

--- zamong/src/utils/api/DiaryWrite/index.ts
+++ zamong/src/utils/api/DiaryWrite/index.ts
@@ -1,23 +1,30 @@
+import { AxiosResponse } from "axios";
 import uri from "../../../constance/uri";
 import { diaryWriteRequest } from "../../../models/dto/request/diaryWriteRequest";
+import { diaryWriteResponse } from "../../../models/dto/response/diaryWriteResponse";
 import { getRequest } from "../default";
 
-export const diaryWritePost = async (data: diaryWriteRequest) => {
+export const diaryWritePost = async (
+  data: diaryWriteRequest
+): Promise<AxiosResponse<diaryWriteResponse>> => {
   const request = getRequest();
   try {
-    const response = await request.post(uri.dreamShare, data);
+    const response = await request.post<diaryWriteResponse>(uri.dreamShare, data);
     return response;
   } catch (error) {
-    return error;
+    return Promise.reject(error);
   }
 };
 
-export const diaryWritePut = async (data: diaryWriteRequest, dreamUUID: string) => {
+export const diaryWritePut = async (
+  data: diaryWriteRequest,
+  dreamUUID: string
+): Promise<AxiosResponse<diaryWriteResponse>> => {
   const request = getRequest();
   try {
-    const response = await request.put(`${uri.dreamShare}/${dreamUUID}`, data);
+    const response = await request.put<diaryWriteResponse>(`${uri.dreamShare}/${dreamUUID}`, data);
     return response;
   } catch (error) {
-    return error;
+    return Promise.reject(error);
   }
 };
