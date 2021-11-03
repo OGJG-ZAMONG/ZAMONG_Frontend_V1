214c68584906d4a9af9f7f6a6f9e76f350422640
wotjd1799 <wotjd0676@gmail.com>
2021-11-03 15:38:47 +0900

    ⚡️ :: (#158) Input
    입력값 저장

--- zamong/src/components/FindPassword/Change/Change.tsx
+++ zamong/src/components/FindPassword/Change/Change.tsx
@@ -1,10 +1,52 @@
+import { stringify } from "querystring";
+import { useEffect, useState } from "react";
 import * as S from "./styles";
 
 type propsType = {
-  prev : () => void;
-}
+  prev: () => void;
+};
 
-const Change = ( { prev } : propsType ) => {
+const Change = ({ prev }: propsType) => {
+  const [inputs, setInputs] = useState({
+    inputPw: "",
+    isPw: false,
+    pwErrorText: "",
+    inputPwCheck: "",
+    isPwCheck: false,
+    pwCheckErrorText: "",
+  });
+
+  const { inputPw, pwErrorText, inputPwCheck, pwCheckErrorText } = inputs;
+
+  const change = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    if (!inputPwCheck.length) {
+      setInputs({
+        ...inputs,
+        pwCheckErrorText: "",
+        isPw: false,
+      });
+    } else if (inputPwCheck !== inputPw) {
+      setInputs({
+        ...inputs,
+        pwCheckErrorText: "비밀번호가 맞지 않습니다.",
+        isPw: false,
+      });
+    } else {
+      setInputs({
+        ...inputs,
+        pwCheckErrorText: "",
+        isPw: true,
+      });
+    }
+  }, [inputPwCheck]);
 
   return (
     <S.ChangeBox>
@@ -12,14 +54,24 @@ const Change = ( { prev } : propsType ) => {
         <S.Title>비밀번호 변경</S.Title>
         <S.GuideContaier>
           <S.GuideWord>비밀번호</S.GuideWord>
-          <S.ErrorMessage></S.ErrorMessage>
+          <S.ErrorMessage>{pwErrorText}</S.ErrorMessage>
         </S.GuideContaier>
-        <S.Input />
+        <S.Input
+          name="inputPw"
+          value={inputPw}
+          type="password"
+          onChange={change}
+        />
         <S.GuideContaier>
           <S.GuideWord>비밀번호 확인</S.GuideWord>
-          <S.ErrorMessage></S.ErrorMessage>
+          <S.ErrorMessage>{pwCheckErrorText}</S.ErrorMessage>
         </S.GuideContaier>
-        <S.Input />
+        <S.Input
+          name="inputPwCheck"
+          value={inputPwCheck}
+          type="password"
+          onChange={change}
+        />
         <S.EventBox>
           <S.PrevButton onClick={prev}>이전</S.PrevButton>
           <S.NextButton>변경</S.NextButton>
--- zamong/src/components/FindPassword/Find/Find.tsx
+++ zamong/src/components/FindPassword/Find/Find.tsx
@@ -1,20 +1,40 @@
 import * as S from "./styles";
+import { useHistory } from "react-router";
+import { useState, useEffect } from "react";
 
 type propsType = {
   next : () => void;
 }
 
 const Find = ( { next } : propsType ) => {
+  const { push } = useHistory();
+  const [inputs, setInputs] = useState({
+    inputEmail: "",
+    isEmail: false,
+  });
 
+  const { inputEmail , isEmail } = inputs;
+
+  const change = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const linkLogin = () => {
+    push("/");
+  }
 
   return (
     <S.FindBox>
       <S.PaddingBox>
         <S.Title>비밀번호 찾기</S.Title>
         <S.GuideWord>이메일</S.GuideWord>
-        <S.Input />
+        <S.Input name="inputEmail" value={inputEmail} onChange={change}/>
         <S.EventBox>
-          <S.PrevButton>이전</S.PrevButton>
+          <S.PrevButton onClick={linkLogin}>이전</S.PrevButton>
           <S.NextButton onClick={next}>다음</S.NextButton>
         </S.EventBox>
       </S.PaddingBox>
