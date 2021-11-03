e7f327a691a20e92e803b4fc9f6ba14fb8f717e5
김진근 <freedom7113@gmail.com>
2021-11-03 12:13:07 +0900

    :memo: :: (#127) move quality array to constance

--- zamong/src/components/DiaryWrite/DiaryWrite.tsx
+++ zamong/src/components/DiaryWrite/DiaryWrite.tsx
@@ -29,29 +29,6 @@ type PropertysType = {
   types: DreamTypeType[];
 };
 
-const qualitys: Code[] = [
-  {
-    code: "BEST",
-    name: "😚 아주 좋아요",
-  },
-  {
-    code: "GOOD",
-    name: "🙂 좋아요",
-  },
-  {
-    code: "SOSO",
-    name: "😐 그저 그래요",
-  },
-  {
-    code: "BAD",
-    name: "☹️ 안좋아요",
-  },
-  {
-    code: "WORST",
-    name: "😬 아주 안좋아요",
-  },
-];
-
 interface PropsType {
   dreamUUID: string | null;
 }
