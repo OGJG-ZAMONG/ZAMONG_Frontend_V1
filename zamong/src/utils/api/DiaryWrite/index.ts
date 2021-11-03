41218cb3d9fb6e1b75fa526cc3b5275d5b57c01b
김진근 <freedom7113@gmail.com>
2021-11-03 10:59:40 +0900

    ⚡️ :: (#127) get access token
    엑세스 토큰을 가지고 와서 요청에 썼다

--- zamong/src/utils/api/DiaryWrite/index.ts
+++ zamong/src/utils/api/DiaryWrite/index.ts
@@ -3,8 +3,6 @@ import uri from "../../../constance/uri";
 import { diaryWriteRequest } from "../../../models/dto/request/diaryWriteRequest";
 import { diaryWriteResponse } from "../../../models/dto/response/diaryWriteResponse";
 import { getRequestWithToken } from "../default";
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzU4NTc0NzYsImV4cCI6MTYzNTg2NDY3Niwic3ViIjoiZTljNzc2YjYtZGE3Yy00ZjM0LWFiNWMtNjA2MGNkNGNiODI4IiwidHlwZSI6ImFjY2VzcyJ9.lCDiWv7xmkHchcg5Ncrg0wVyOUeIEZ9ZCXKBc56wcrQ";
 
 export type diaryWriteApiType = (
   data: diaryWriteRequest,
@@ -15,7 +13,8 @@ export const diaryWritePost = async (
   data: diaryWriteRequest,
   dreamUUID: string
 ): Promise<AxiosResponse<diaryWriteResponse>> => {
-  const request = getRequestWithToken(token);
+  const token = localStorage.getItem("access_token");
+  const request = getRequestWithToken(token!);
   try {
     const response = await request.post<diaryWriteResponse>(uri.dreamShare, data);
     return response;
@@ -28,7 +27,8 @@ export const diaryWritePut = async (
   data: diaryWriteRequest,
   dreamUUID: string
 ): Promise<AxiosResponse<diaryWriteResponse>> => {
-  const request = getRequestWithToken(token);
+  const token = localStorage.getItem("access_token");
+  const request = getRequestWithToken(token!);
   try {
     const response = await request.put<diaryWriteResponse>(`${uri.dreamShare}/${dreamUUID}`, data);
     return response;
@@ -41,7 +41,8 @@ export const diaryWriteFilePost = async (
   file: File,
   dreamUUID: string
 ): Promise<AxiosResponse<any>> => {
-  const request = getRequestWithToken(token);
+  const token = localStorage.getItem("access_token");
+  const request = getRequestWithToken(token!);
   try {
     const formData = new FormData();
     formData.append("file", file);
