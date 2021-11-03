62fc51cb663b4e458fe6e029fb721a4bd026c8b5
김진근 <freedom7113@gmail.com>
2021-11-03 17:22:38 +0900

    :memo: :: (#127) get token

--- zamong/src/utils/api/DreamDetail/index.ts
+++ zamong/src/utils/api/DreamDetail/index.ts
@@ -3,9 +3,7 @@ import { dreamDetailResponse } from "../../../models/dto/response/dreamDetailRes
 import { getRequestWithToken } from "../default";
 
 export const getDreamDetail = async (dreamUUID: string) => {
-  //   const token = localStorage.getItem("access_token");
-  const token =
-    "eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MzU5MTM1NDMsImV4cCI6MTYzNTkyMDc0Mywic3ViIjoiZTljNzc2YjYtZGE3Yy00ZjM0LWFiNWMtNjA2MGNkNGNiODI4IiwidHlwZSI6ImFjY2VzcyJ9.71ENgFg600qev4xyPlAEnoT-QQRhcqR81Pe216ZdWig";
+  const token = localStorage.getItem("access_token");
   const request = getRequestWithToken(token!);
   try {
     const response = await request.get<dreamDetailResponse>(`${uri.dreamShare}/${dreamUUID}`);
