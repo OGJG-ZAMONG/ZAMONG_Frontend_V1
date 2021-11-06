5232388119d93f6bb45e85971f5a57abc0d959ff
김진근 <freedom7113@gmail.com>
2021-11-06 13:26:26 +0900

    ⚡️ :: (#178) column gap props
    gap 변수를 style에도 적용했다

--- zamong/src/components/MainPage/Slider/styles.ts
+++ zamong/src/components/MainPage/Slider/styles.ts
@@ -6,9 +6,9 @@ export const Container = styled.div`
   overflow: hidden;
 `;
 
-export const List = styled.div<{ x: number }>`
+export const List = styled.div<{ x: number; gap: number }>`
   display: flex;
-  column-gap: 20px;
+  column-gap: ${(props) => props.gap}px;
   transform: translateX(${(props) => props.x}px);
   transition: transform 0.5s cubic-bezier(0.19, 1, 0.22, 1);
 `;
