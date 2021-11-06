22d628101e37889b1e1f161c9122b62339e08d39
김진근 <freedom7113@gmail.com>
2021-11-06 15:15:13 +0900

    :memo: :: (#168) change Component Name
    기능에 맞지 않던 파일들의 이름을 바꾸었다

--- zamong/src/components/Header/NonLoginComponent/NonLoginComponent.tsx
+++ zamong/src/components/Header/NonLoginComponent/NonLoginComponent.tsx
@@ -1,39 +1,10 @@
-import { BorderButton } from "../styles";
-import * as S from "./styles";
-import defaultImg from "../../../assets/DefaultPostingImages/1.jpg";
-import Chat from "../../../assets/icons/Chat.svg";
-import Coin from "../../../assets/icons/Coin.svg";
-import Discovery from "../../../assets/icons/Discovery.svg";
-import ShoppingCart from "../../../assets/icons/ShoppingCart.svg";
+import * as S from "../styles";
 
 const NonLoginComponent = (): JSX.Element => {
   return (
     <>
-      <S.NavigationContainer>
-        <S.LinksContainer>
-          <S.LinkComponentContainer to="/">
-            <img alt="chat" src={Chat} />
-            <span>채팅</span>
-          </S.LinkComponentContainer>
-          <S.LinkComponentContainer to="/">
-            <img alt="Discovery" src={Discovery} />
-            <span>꿈 해몽</span>
-          </S.LinkComponentContainer>
-          <S.LinkComponentContainer to="/">
-            <img alt="Coin" src={Coin} />
-            <span>꿈 구매</span>
-          </S.LinkComponentContainer>
-          <S.LinkComponentContainer to="/">
-            <img alt="ShoppingCart" src={ShoppingCart} />
-            <span>꿈 판매</span>
-          </S.LinkComponentContainer>
-        </S.LinksContainer>
-        <S.UserProfileContainer>
-          <S.UserProfileImg alt="user-img" src={defaultImg} />
-          <span>USER04</span>
-        </S.UserProfileContainer>
-      </S.NavigationContainer>
-      <BorderButton>로그아웃</BorderButton>
+      <S.BlueButton>로그인</S.BlueButton>
+      <S.BorderButton>회원가입</S.BorderButton>
     </>
   );
 };
