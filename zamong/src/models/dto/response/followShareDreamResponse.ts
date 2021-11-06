ca5e86041688bd1ab077ec0300e295b830a52add
김진근 <freedom7113@gmail.com>
2021-11-06 22:13:15 +0900

    :memo: :: (#169) divide interface
    팔로우 공유 꿈 인터페이스에 꿈 부분을 따로 사용할 일이 있기때문에 빼주었다

--- zamong/src/models/dto/response/followShareDreamResponse.ts
+++ zamong/src/models/dto/response/followShareDreamResponse.ts
@@ -1,20 +1,20 @@
+export interface Dream {
+  uuid: string;
+  default_posting_image: string;
+  profile: string;
+  title: string;
+  lucy_count: number;
+  dream_types: string[];
+  share_datetime: string;
+}
+
 export interface followShareDreamResponse {
   status: number;
   timestamp: string;
   content: {
     collection_value: boolean;
     response: {
-      share_dreams: [
-        {
-          uuid: string;
-          default_posting_image: string;
-          profile: string;
-          title: string;
-          lucy_count: number;
-          dream_types: string[];
-          share_datetime: string;
-        }
-      ];
+      share_dreams: Dream[];
       total_page: number;
       total_size: number;
     };
