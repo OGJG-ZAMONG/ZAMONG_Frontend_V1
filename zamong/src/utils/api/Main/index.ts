e2cfb4997392905343c65cde1705c65bb5013b34
김진근 <freedom7113@gmail.com>
2021-11-08 11:55:45 +0900

    ⚡️ :: (#195) add follow list api

--- zamong/src/utils/api/Main/index.ts
+++ zamong/src/utils/api/Main/index.ts
@@ -3,36 +3,49 @@ import { shareDreamRequest } from "../../../models/dto/request/shareDreamRequest
 import { myShareDreamRequest } from "../../../models/dto/request/myShareDreamRequest";
 import { shareDreamResponse } from "../../../models/dto/response/shareDreamResponse";
 import { getRequestWithToken } from "../default";
+import { followrResponse } from "../../../models/dto/response/followerResponse";
 
 export const getFollowShareDream = async (param: shareDreamRequest) => {
   const token = localStorage.getItem("access_token");
   const request = getRequestWithToken(token!);
   try {
-    const response = request.get<shareDreamResponse>(uri.dreamShareFollow, { params: param });
+    const response = await request.get<shareDreamResponse>(uri.dreamShareFollow, { params: param });
     return response;
   } catch (error) {
     return Promise.reject(error);
   }
 };
 
-export const getMyDreamDiary = (param: myShareDreamRequest) => {
+export const getMyDreamDiary = async (param: myShareDreamRequest) => {
   const token = localStorage.getItem("access_token");
   const request = getRequestWithToken(token!);
 
   try {
-    const response = request.get<shareDreamResponse>(uri.dreamShareMe, { params: param });
+    const response = await request.get<shareDreamResponse>(uri.dreamShareMe, { params: param });
     return response;
   } catch (error) {
     return Promise.reject(error);
   }
 };
 
-export const getShareDream = (param: shareDreamRequest) => {
+export const getShareDream = async (param: shareDreamRequest) => {
   const token = localStorage.getItem("access_token");
   const request = getRequestWithToken(token!);
 
   try {
-    const response = request.get<shareDreamResponse>(uri.dreamShare, { params: param });
+    const response = await request.get<shareDreamResponse>(uri.dreamShare, { params: param });
+    return response;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const getFollowList = async (param: { page: number; size: number }) => {
+  const token = localStorage.getItem("access_token");
+  const request = getRequestWithToken(token!);
+
+  try {
+    const response = await request.get<followrResponse>(uri.follower, { params: param });
     return response;
   } catch (error) {
     return Promise.reject(error);
