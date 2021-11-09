4ab77f2470750382083d4699b2fb273e007bcf53
김진근 <freedom7113@gmail.com>
2021-11-09 22:39:29 +0900

    ⚡️ :: (#167) Interconnecting API

--- zamong/src/components/MainPage/DreamList/DreamList.tsx
+++ zamong/src/components/MainPage/DreamList/DreamList.tsx
@@ -2,23 +2,54 @@ import * as S from "./styles";
 import * as I from "../styles";
 import Dream from "../../Dream/Dream";
 import { DownChevron } from "../../../assets";
+import { getShareDream } from "../../../utils/api/Main";
+import { useEffect, useState } from "react";
+import { Dream as DreamType } from "../../../models/dto/response/shareDreamResponse";
+import { shareDreamRequest } from "../../../models/dto/request/shareDreamRequest";
 
 const DreamList = (): JSX.Element => {
+  const [dreams, setDreams] = useState<DreamType[]>([]);
+  const [page, setPage] = useState<number>(0);
+
+  const setDreamList = async () => {
+    const param: shareDreamRequest = {
+      page: page,
+      size: 5,
+    };
+
+    try {
+      const { share_dreams } = (await getShareDream(param)).data.content.response;
+      setDreams(dreams.concat(share_dreams));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onMore = () => {
+    setPage(page + 1);
+  };
+
+  const dreamsRender = dreams.map((value) => {
+    return <Dream />;
+  });
+
+  useEffect(() => {
+    setDreamList();
+  }, [page]);
+
   return (
-    <div>
-      <I.SectionTitle>공개된 꿈 목록</I.SectionTitle>
-      <div>
-        <Dream />
-        <Dream />
-        <Dream />
-        <Dream />
-        <Dream />
-      </div>
-      <S.MoreContaier>
-        <div>더보기</div>
-        <img alt="down" src={DownChevron} />
-      </S.MoreContaier>
-    </div>
+    <>
+      {dreams.length > 0 && (
+        <div>
+          <I.SectionTitle>공개된 꿈 목록</I.SectionTitle>
+          <div>{dreamsRender}</div>
+          <S.MoreContaier onClick={onMore}>
+            <div>더보기</div>
+            <img alt="down" src={DownChevron} />
+          </S.MoreContaier>
+        </div>
+      )}
+    </>
   );
 };
 
