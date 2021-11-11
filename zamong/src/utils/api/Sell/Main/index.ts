8e81c0be1390c1edcf0c5b8ee9d0e2f6d6159861
Jun Joy <chojuneseo1@gmail.com>
2021-11-11 19:54:55 +0900

    ⚡️ :: (#204) Get Current Selling Dreams Api

---
+++ zamong/src/utils/api/Sell/Main/index.ts
@@ -0,0 +1,14 @@
+import { getRequestWithToken } from "../../default/index";
+export const getCurrentSellingDreams = async (
+  access_token: any,
+  page: number
+) => {
+  try {
+    const request = getRequestWithToken(access_token, "json").get(
+      `/dream/sell/continue?page=${page}&size=10`
+    );
+    return await request;
+  } catch (error) {
+    throw error;
+  }
+};
