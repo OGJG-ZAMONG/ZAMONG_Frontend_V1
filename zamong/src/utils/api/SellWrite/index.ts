d435ce1246410b1e259da6987cbfdf41290c9ff7
김진근 <freedom7113@gmail.com>
2021-11-15 10:09:10 +0900

    ⚡️ :: (#214) change api structure

--- zamong/src/utils/api/SellWrite/index.ts
+++ zamong/src/utils/api/SellWrite/index.ts
@@ -1,13 +1,14 @@
 import uri from "../../../constance/uri";
 import { sellWriteResponse } from "../../../models/dto/request/sellWriteResquest";
+import { diaryWriteResponse } from "../../../models/dto/response/diaryWriteResponse";
 import instance from "../axios";
 
 export const postSellDream = async (data: sellWriteResponse) => {
   try {
-    await instance.post(uri.dreamSell, data);
-    return true;
+    const response = await instance.post<diaryWriteResponse>(uri.dreamSell, data);
+    return response;
   } catch (error) {
     console.log(error);
-    return false;
+    return Promise.reject(error);
   }
 };
