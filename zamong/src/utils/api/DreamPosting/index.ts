3d1a2c3de9f1f3ebc171f42f2b0a46c3b73eba77
김진근 <freedom7113@gmail.com>
2021-11-15 13:08:21 +0900

    ⚡️ :: (#226) change import

--- zamong/src/utils/api/DreamPosting/index.ts
+++ zamong/src/utils/api/DreamPosting/index.ts
@@ -1,13 +1,11 @@
 import { AxiosResponse } from "axios";
 import uri from "../../../constance/uri";
-import { request } from "../axios";
-import { getRequestWithToken } from "../default";
+import request from "../axios";
 
 export const dreamPostingImagePost = async (
   file: File,
   dreamUUID: string
 ): Promise<AxiosResponse<any>> => {
-  const token = localStorage.getItem("access_token");
   try {
     const formData = new FormData();
     formData.append("file", file);
