0a68b0009a4a1da14096d40a4c2efd87b26eefdf
김진근 <freedom7113@gmail.com>
2021-11-17 12:10:34 +0900

    ⚡️ :: (#250) move button

--- zamong/src/components/MainPage/Slider/Slider.tsx
+++ zamong/src/components/MainPage/Slider/Slider.tsx
@@ -1,18 +1,47 @@
 import { useLayoutEffect, useRef, useState } from "react";
+import { State } from "../../DiaryWrite/model";
 import * as S from "./styles";
 
 interface PropsType {
   children: React.ReactNode;
-  index: number;
   size: number;
   columnCount: number;
   gap: number;
+  indexState: State<number>;
+  pageState: State<number>;
 }
 
-const Slider = ({ children, index, size, gap, columnCount }: PropsType) => {
+const Slider = ({ children, size, gap, columnCount, indexState, pageState }: PropsType) => {
+  const [index, setIndex] = indexState;
+  const [page, setPage] = pageState;
+
   const container = useRef<HTMLDivElement>(null);
   const list = useRef<HTMLDivElement>(null);
   const [x, setX] = useState<number>(0);
+
+  const moveIndex = (offset: number) => {
+    const changeIndex = index + offset;
+
+    if (changeIndex < 0 || changeIndex > size - columnCount) {
+      return;
+    }
+
+    if (changeIndex > size - columnCount - 1) {
+      setPage(page + 1);
+    }
+
+    setIndex(changeIndex);
+  };
+
+  const onNext = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    moveIndex(1);
+  };
+
+  const onPrev = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    moveIndex(-1);
+  };
 
   useLayoutEffect(() => {
     const itemWidth = (container.current?.clientWidth! - (columnCount - 1) * gap) / columnCount;
