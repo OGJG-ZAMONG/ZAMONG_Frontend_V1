8b0cb924603af6652beb2a83d08b406f38fd0e70
김진근 <freedom7113@gmail.com>
2021-11-29 15:50:49 +0900

    ⚡️ :: (#217) add on scroll hander

--- zamong/src/components/Background/Background.tsx
+++ zamong/src/components/Background/Background.tsx
@@ -15,6 +15,35 @@ interface PropsType {
     left: number;
     top: number;
   }
+
+  const [imageList, setImageList] = useState<Delusional[]>([]);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const lastYRef = useRef<number>(startY);
+  const isLeftRef = useRef<boolean>(false);
+
+  const { current: lastY } = lastYRef;
+  const { current: isLeft } = isLeftRef;
+  const offset = window.screen.height * 0.8;
+
+  const onScrollHandler = () => {
+    const screenBottom = window.scrollY + window.screen.height;
+
+    if (screenBottom >= lastY + offset) {
+      const id = getRandomInt(1, 21).toString();
+      const left = isLeft ? 0 : 100;
+      const img = import(`../../assets/delusional/delusional${id}.png`);
+
+      lastYRef.current = screenBottom;
+      isLeftRef.current = !isLeft;
+
+      setImageList(imageList.concat({ left: left, url: img, top: screenBottom }));
+    }
+
+    if (containerRef.current) {
+      containerRef.current.style.top = `${-window.scrollY}px`;
+    }
+  };
 };
 
 export default Background;
