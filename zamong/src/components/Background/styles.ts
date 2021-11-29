7a6152e1193f8b95b02c18e629d2552c2a44e7a0
김진근 <freedom7113@gmail.com>
2021-11-29 15:54:41 +0900

    ⚡️ :: (#217) add Delusional image style

--- zamong/src/components/Background/styles.ts
+++ zamong/src/components/Background/styles.ts
@@ -4,7 +4,6 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   z-index: -1;
-  background-color: red;
   position: fixed;
   top: 0px;
   left: 0px;
@@ -16,4 +15,19 @@ export const Inner = styled.div`
   height: 100%;
   position: relative;
   transition: top 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+`;
+
+export const Delusional = styled.div<{ left: number; top: number; isActive: boolean }>`
+  left: ${(props) => props.left}%;
+  top: ${(props) => props.top}px;
+  position: absolute;
+  contain: paint;
+  transform: translate(-50%, -${(props) => (props.isActive ? 50 : 55)}%);
+  opacity: ${(props) => (props.isActive ? 1 : 0)};
+  transition: transform 1s cubic-bezier(0.075, 0.82, 0.165, 1),
+    opacity 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  img {
+    object-fit: cover;
+    object-position: center;
+  }
 `;
