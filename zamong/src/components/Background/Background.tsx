65a86ed4c21ab568251e7483d904646e4ac71398
김진근 <freedom7113@gmail.com>
2021-11-29 15:49:05 +0900

    ⚡️ :: (#217) add random function

--- zamong/src/components/Background/Background.tsx
+++ zamong/src/components/Background/Background.tsx
@@ -4,6 +4,12 @@ interface PropsType {
   startY: number;
       
   </S.Container>;
+  const getRandomInt = (min: number, max: number) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+  };
+
 };
 
 export default Background;
