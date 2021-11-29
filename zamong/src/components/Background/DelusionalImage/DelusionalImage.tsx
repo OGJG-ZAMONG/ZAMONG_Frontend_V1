02e0d4389248868371ec6c90969ef6783014c189
김진근 <freedom7113@gmail.com>
2021-11-29 15:53:39 +0900

    ⚡️ :: (#217) add DelusionalImage component

---
+++ zamong/src/components/Background/DelusionalImage/DelusionalImage.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+import * as S from "../styles";
+
+interface PropsType {
+  url: Promise<any>;
+  left: number;
+  top: number;
+}
+
+const DelusionalImage = ({ left, top, url }: PropsType): JSX.Element => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [imgUrl, setImgUrl] = useState<string>("");
+
+  useEffect(() => {
+    console.log(isActive);
+
+    url.then((value) => {
+      setImgUrl(value.default);
+      setIsActive(true);
+      console.log(value.default);
+    });
+  }, [isActive]);
+
+  return (
+    <S.Delusional ref={containerRef} left={left} top={top} isActive={isActive}>
+      <img alt="Delusional" src={imgUrl} />
+    </S.Delusional>
+  );
+};
+
+export default DelusionalImage;
