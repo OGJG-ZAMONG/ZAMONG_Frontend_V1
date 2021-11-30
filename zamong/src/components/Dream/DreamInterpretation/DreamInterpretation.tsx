bf767b3d0bb6535acedf8d0a0fdbd0c6c0ddf5a0
김진근 <freedom7113@gmail.com>
2021-12-01 01:08:23 +0900

    ⚡️ :: (#312) use Dream Interpretation component

--- zamong/src/components/Dream/DreamInterpretation/DreamInterpretation.tsx
+++ zamong/src/components/Dream/DreamInterpretation/DreamInterpretation.tsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import dreamType from "../../../constance/dreamType";
+import * as I from "../styles";
+import Default from "../../../assets/DefaultPostingImages/1.jpg";
+import Tag from "../../Tag/Tag";
+
 const DreamInterpretation = (): JSX.Element => {
-  return <div></div>;
+  const [isUserImageHover, setIsUserImageHover] = useState<boolean>(false);
+
+  const dateToString = (date: Date) => {
+    if (date.getFullYear() !== new Date().getFullYear())
+      return `${date.getFullYear()}-${date.getMonth() + 1}=${date.getDate()}`;
+
+    return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+  };
+
+  return (
+    <>
+      <I.DreamContainer>
+        <I.DreamImage img={Default}>
+          <I.DreamUserImage
+            alt="user image"
+            src={Default}
+            onMouseEnter={() => {
+              setIsUserImageHover(true);
+            }}
+            onMouseLeave={() => {
+              setIsUserImageHover(false);
+            }}
+          />
+          {isUserImageHover && <I.UserName>kjg2004</I.UserName>}
+        </I.DreamImage>
+        <I.DreamInfoContainer>
+          <I.DreamTitle>안녕하세요 이거 해몽해주세요.</I.DreamTitle>
+          <I.DreamLucy>21 LUCY 지급</I.DreamLucy>
+          <I.DreamDate>8월 1일</I.DreamDate>
+        </I.DreamInfoContainer>
+        <I.DreamTagContainer>
+          <I.DreamTagInner>
+            <Tag>악몽</Tag>
+            <Tag>첫 게시물</Tag>
+            <Tag>루시드 드림</Tag>
+            <Tag>거짓 깨어남</Tag>
+          </I.DreamTagInner>
+        </I.DreamTagContainer>
+      </I.DreamContainer>
+      <I.Line></I.Line>
+    </>
+  );
 };
 
 export default DreamInterpretation;
--- zamong/src/components/DreamInterpretationMain/DreamInterpretationList/DreamInterpretationList.tsx
+++ zamong/src/components/DreamInterpretationMain/DreamInterpretationList/DreamInterpretationList.tsx
@@ -1,9 +1,15 @@
+import DreamInterpretation from "../../Dream/DreamInterpretation/DreamInterpretation";
 import * as S from "./styles";
 
 const DreamInterpretationList = (): JSX.Element => {
   return (
     <S.ContentInner>
       <S.Subtitle>해몽 요청</S.Subtitle>
+      <div>
+        {[1, 2, 3, 4, 5].map((_, index) => {
+          return <DreamInterpretation />;
+        })}
+      </div>
     </S.ContentInner>
   );
 };
