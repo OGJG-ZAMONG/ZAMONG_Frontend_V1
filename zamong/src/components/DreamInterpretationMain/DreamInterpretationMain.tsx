48664e81388dd800f40bb6f972f3a73794925f74
김진근 <freedom7113@gmail.com>
2021-11-30 18:47:14 +0900

    ⚡️ :: (#312) add Default Dream Interpretation

--- zamong/src/components/DreamInterpretationMain/DreamInterpretationMain.tsx
+++ zamong/src/components/DreamInterpretationMain/DreamInterpretationMain.tsx
@@ -1,5 +1,15 @@
+import DefaultDreamInterpretation from "./DefaultDreamInterpretation/DefaultDreamInterpretation";
+import * as S from "./styles";
+
 const DreamInterpretationMain = (): JSX.Element => {
-  return <div></div>;
+  return (
+    <S.Container>
+      <S.Title>해몽</S.Title>
+      <S.ContentContainer>
+        <DefaultDreamInterpretation />
+      </S.ContentContainer>
+    </S.Container>
+  );
 };
 
 export default DreamInterpretationMain;
--- zamong/src/components/DreamInterpretationMain/styles.ts
+++ zamong/src/components/DreamInterpretationMain/styles.ts
@@ -1,1 +1,22 @@
-export {};
+import styled from "@emotion/styled";
+import { color } from "../../style/color";
+import { font } from "../../style/font";
+
+export const Container = styled.div`
+  width: 1280px;
+  margin: 0px auto;
+  box-sizing: border-box;
+  color: ${color.white};
+`;
+
+export const Title = styled.div`
+  font: ${font.headline1};
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 86px;
+  margin-top: 48px;
+`;
