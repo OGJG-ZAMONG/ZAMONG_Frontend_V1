f89abf4ba909ff8083c9344f2120c14415530fcd
kky04994 <kkyo8747@naver.com>
2021-11-30 22:46:54 +0900

    ⚡️ :: (#186) Btn 스타일 추가

--- zamong/src/components/Profile/Account/style.ts
+++ zamong/src/components/Profile/Account/style.ts
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+import { font } from "../../../style/font";
 
 export const Content = styled.div`
   width: 100%;
@@ -8,7 +10,6 @@ export const Content = styled.div`
 export const ProfileBox = styled.div`
   width: 1279px;
   height: 119px;
-  border-bottom: 1px solid white;
   div {
     display: flex;
     justify-content: space-between;
@@ -24,13 +25,39 @@ export const TitleText = styled.p`
   margin-bottom: 44px;
 `;
 
-export const SubTitle = styled.span`
+export const SubTitle = styled.label`
   font-family: Spoqa Han Sans Neo;
   font-size: 16px;
   color: #ffffff;
 `;
 
-export const ChangeBtn = styled.button`
+export const FileBox = styled.div`
+  display: inline-block;
+  margin-right: 10px;
+  align-items: center;
+  border-radius: 120px;
+`;
+
+export const FileText = styled.label`
+  width: 62px;
+  height: 31px;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 120px;
+  text-align: center;
+  border: 1px solid white;
+  background-color: black;
+  font: ${font.body2};
+  padding-top: 2px;
+`;
+
+export const FileBtn = styled.input`
+  position: absolute;
+  clip: rect(0, 0, 0, 0);
+  border: 0;
+`;
+
+export const ChangeBtn = styled.div`
   width: 62px;
   height: 31px;
   border: 1px solid white;
@@ -38,7 +65,47 @@ export const ChangeBtn = styled.button`
   border-radius: 120px;
   color: #ffffff;
   cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  align-items: center;
+  font-size: 16px;
+  padding-top: 2px;
+  margin-left: 16px;
 `;
+
+export const CancelBtn = styled.div`
+  width: 62px;
+  height: 31px;
+  border: 1px solid #ff453a;
+  background-color: black;
+  border-radius: 120px;
+  color: #ff453a;
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  align-items: center;
+  font-size: 16px;
+  padding-top: 2px;
+  margin-left: 9px;
+`;
+
+export const ChangePwBtn = styled(Link)`
+  width: 62px;
+  height: 31px;
+  border: 1px solid white;
+  background-color: black;
+  border-radius: 120px;
+  color: #ffffff;
+  text-decoration: none;
+  cursor: pointer;
+  display: inline-block;
+  text-align: center;
+  align-items: center;
+  font-size: 16px;
+  padding-top: 2px;
+`;
+
+export const IdBox = styled.div``;
 
 export const AccountBox = styled.div`
   width: 1279px;
@@ -59,6 +126,22 @@ export const IdText = styled.span`
   margin-right: 18px;
 `;
 
+export const inputId = styled.input`
+  width: 247px;
+  height: 30px;
+  background: #636366;
+  border-radius: 10px;
+  border: none;
+  color: #ffffff;
+  outline: none;
+  text-align: right;
+  padding: 0px 16px;
+  :hover {
+    outline: none;
+    border: none;
+  }
+`;
+
 export const DeleteText = styled.span`
   color: #ff453a;
   font-family: Spoqa Han Sans Neo;
@@ -73,4 +156,6 @@ export const DeleteBtn = styled.button`
   border-radius: 120px;
   color: #ff453a;
   cursor: pointer;
+  font-size: 16px;
+  padding-top: 1px;
 `;
