b5c53ad8303087fefacaddd97c93bb1a5765ed95
김진근 <freedom7113@gmail.com>
2021-12-01 01:43:00 +0900

    ⚡️ :: (#314) write api connect

--- zamong/src/utils/api/DreamInterpretationMain/index.ts
+++ zamong/src/utils/api/DreamInterpretationMain/index.ts
@@ -1,3 +1,15 @@
-export const getDreamInterpretationList = (page: number, size: number) => {
-    
+import uri from "../../../constance/uri";
+import instance from "../axios";
+
+export const getDreamInterpretationList = async (page: number, size: number) => {
+  try {
+    return await instance.get(uri.interpretation, {
+      params: {
+        page: page,
+        size: size,
+      },
+    });
+  } catch (error) {
+    return Promise.reject(error);
+  }
 };
