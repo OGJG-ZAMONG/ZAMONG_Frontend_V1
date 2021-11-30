5ee9a6c3ad74cf4c3d7ca994bda7295fb0783f1e
김진근 <freedom7113@gmail.com>
2021-11-30 22:30:37 +0900

    ⚡️ :: (#312) add on click handler

--- zamong/src/components/CardDream/DefaultDreamInterpretationCard/DefaultDreamInterpretationCard.tsx
+++ zamong/src/components/CardDream/DefaultDreamInterpretationCard/DefaultDreamInterpretationCard.tsx
@@ -1,14 +1,19 @@
 import DefaultDreamInterpretationType from "../../../interface/DefaultDreamInterpretationType";
 import * as I from "../index";
+import * as S from "./styles";
 
 export const DefaultDreamInterpretationCard = ({
   image,
   title,
   content,
 }: DefaultDreamInterpretationType) => {
+  const onClickHandler = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    e.preventDefault();
+  };
+
   return (
-    <I.DreamCardContainer to="/">
-      <I.DreamImageContainer img={image}>{content}</I.DreamImageContainer>
+    <I.DreamCardContainer to="" onClick={onClickHandler}>
+      <I.DreamImageContainer img={image}></I.DreamImageContainer>
       <I.DreamTitle>{title}</I.DreamTitle>
     </I.DreamCardContainer>
   );
