d52b6464bb655d28ba1108fe87679a5e52d339ed
김진근 <freedom7113@gmail.com>
2021-11-30 18:56:25 +0900

    ⚡️ :: (#312) add props

--- zamong/src/components/CardDream/DefaultDreamInterpretationCard/DefaultDreamInterpretationCard.tsx
+++ zamong/src/components/CardDream/DefaultDreamInterpretationCard/DefaultDreamInterpretationCard.tsx
@@ -1,3 +1,9 @@
-export const DefaultDreamInterpretationCard = () => {};
+interface PropsType {
+  image: string;
+  title: string;
+  content: string;
+}
+
+export const DefaultDreamInterpretationCard = ({ image, title, content }: PropsType) => {};
 
 export default DefaultDreamInterpretationCard;
