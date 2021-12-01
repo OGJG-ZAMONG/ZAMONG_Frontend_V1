da636adf5283cde9c92acbb35af611f534495445
김진근 <freedom7113@gmail.com>
2021-12-02 02:01:21 +0900

    ⚡️ :: (#323) add props

--- zamong/src/components/PageNation/PageNation.tsx
+++ zamong/src/components/PageNation/PageNation.tsx
@@ -1,4 +1,12 @@
-const PageNation = (): JSX.Element => {
+import State from "../../interface/State";
+
+interface PropsType {
+  indexState: State<number>;
+  maxPage: number;
+  columnCount: number;
+}
+
+const PageNation = ({ indexState, maxPage, columnCount }: PropsType): JSX.Element => {
   return <div></div>;
 };
 
