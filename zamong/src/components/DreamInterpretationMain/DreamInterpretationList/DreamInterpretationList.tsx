233727ba805bc49242d9a68e0ecb1c47da17dac5
김진근 <freedom7113@gmail.com>
2021-12-01 21:39:20 +0900

    ⚡️ :: (#313) use api connect

--- zamong/src/components/DreamInterpretationMain/DreamInterpretationList/DreamInterpretationList.tsx
+++ zamong/src/components/DreamInterpretationMain/DreamInterpretationList/DreamInterpretationList.tsx
@@ -1,14 +1,28 @@
 import DreamInterpretation from "../../Dream/DreamInterpretation/DreamInterpretation";
 import * as S from "./styles";
 import DownChevron from "../../../assets/icons/downChevron.svg";
+import { useState } from "react";
+import { InterpretationDream } from "../../../models/dto/response/InterpretationListResponse";
+import { getDreamInterpretationList } from "../../../utils/api/DreamInterpretationMain";
 
 const DreamInterpretationList = (): JSX.Element => {
+  const [interpretations, setInterpretations] = useState<InterpretationDream[]>([]);
+
+  const settingInterpretations = async () => {
+    try {
+      const response = await getDreamInterpretationList(0, 5);
+      setInterpretations(response.data.content.response.interpretation_dreams);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <S.ContentInner>
       <S.Subtitle>해몽 요청</S.Subtitle>
       <div>
         {[1, 2, 3, 4, 5].map((_, index) => {
-          return <DreamInterpretation />;
+          return <DreamInterpretation key={index} />;
         })}
         <S.MoreContaier>
           <div>더보기</div>
