c3b5034fdf67bbcf8369c3b36941f66fe95a2e07
김진근 <freedom7113@gmail.com>
2021-12-01 21:34:50 +0900

    ⚡️ :: (#313) write response file

--- zamong/src/models/dto/response/InterpretationListResponse.ts
+++ zamong/src/models/dto/response/InterpretationListResponse.ts
@@ -1,1 +1,24 @@
-    
+import defaultResponse from "./defaultResponse";
+
+export interface InterpretationDream {
+  uuid: string;
+  default_posting_image: string;
+  title: string;
+  dream_types: string[];
+  updated_at: string;
+  user: {
+    uuid: string;
+    id: string;
+    profile: string;
+  };
+}
+
+export interface InterpretationList {
+  interpretation_dreams: InterpretationDream[];
+  total_page: 1;
+  total_size: 2;
+}
+
+type interpretationListResponse = defaultResponse<InterpretationList>;
+
+export default interpretationListResponse;
