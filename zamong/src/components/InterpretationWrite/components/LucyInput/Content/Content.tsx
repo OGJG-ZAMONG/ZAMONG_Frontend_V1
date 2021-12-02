423f345eabe6d28fcf98e77fa52771f6f4579b8f
김진근 <freedom7113@gmail.com>
2021-12-03 01:07:34 +0900

    ⚡️ :: (#370) use style

--- zamong/src/components/InterpretationWrite/components/LucyInput/Content/Content.tsx
+++ zamong/src/components/InterpretationWrite/components/LucyInput/Content/Content.tsx
@@ -1,5 +1,35 @@
-const Content = (): JSX.Element => {
-  return <div></div>;
+import * as S from "../styles";
+
+interface PropsType {
+  lucy: number;
+  setLucy: (lucy: number) => void;
+}
+
+const Content = ({ lucy, setLucy }: PropsType): JSX.Element => {
+  const isNumeric = (data: string): boolean => {
+    return !isNaN(Number(data));
+  };
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+
+    if (isNumeric(value)) {
+      if (value === "") {
+        setLucy(0);
+      } else {
+        setLucy(parseInt(value));
+      }
+    }
+  };
+
+  return (
+    <>
+      <S.Container>
+        <S.Input placeholder="지급 LUCY 입력..." value={lucy} onChange={onChangeHandler} />
+        <S.Won>₩</S.Won>
+      </S.Container>
+    </>
+  );
 };
 
 export default Content;
