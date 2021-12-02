56a86b68e81d204a6ea0a8f27b4092b4fe4e1f6f
김진근 <freedom7113@gmail.com>
2021-12-03 02:28:12 +0900

    ⚡️ :: (#372) put api

--- zamong/src/utils/api/InterpretationWrite/index.ts
+++ zamong/src/utils/api/InterpretationWrite/index.ts
@@ -3,9 +3,17 @@ import { postInterpretationRequest } from "../../../models/dto/request/postInter
 import { interpretationDetailResponse } from "../../../models/dto/response/interpretationDetailResponse";
 import instance from "../axios";
 
-export const postInterpretation = async (data: postInterpretationRequest) => {
+export const postInterpretation = async (data: postInterpretationRequest, uuid: string) => {
   try {
     return await instance.post(uri.interpretation, data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const putInterpretation = async (data: postInterpretationRequest, uuid: string) => {
+  try {
+    return await instance.put(`${uri.interpretation}/${uuid}`, data);
   } catch (error) {
     return Promise.reject(error);
   }
