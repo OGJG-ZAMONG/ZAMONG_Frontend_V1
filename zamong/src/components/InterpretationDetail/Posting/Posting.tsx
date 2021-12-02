0ed1198e28baedfe4a9bf4eba2982161ab9751cf
LeeJaeSeoung <wotjd0676@gmail.com>
2021-12-03 03:45:59 +0900

    ⚡️ :: (#383) Interpretation Detail Posting
    글부분 완성

--- zamong/src/components/InterpretationDetail/Posting/Posting.tsx
+++ zamong/src/components/InterpretationDetail/Posting/Posting.tsx
@@ -1,12 +1,34 @@
 import * as S from "./styles";
 import { InterpretationDetail } from "../../../models/dto/response/InterpretationDetail";
+import { useEffect, useState } from "react";
 
 interface PropsTypes {
   postData: InterpretationDetail;
 }
 
 const InterpretationDetailPosting = ({ postData }: PropsTypes) => {
-  return <div></div>;
+  const { content, attachment_image, user, uuid } = postData;
+  const [isImg, setIsImg] = useState(false);
+
+  useEffect(() => {
+    if (
+      attachment_image !==
+      "https://ogjg-zamong.s3.ap-northeast-1.amazonaws.com/default-posting-image/Rectangle+1.png"
+    ) {
+      setIsImg(true);
+    } else {
+      setIsImg(false);
+    }
+  }, [postData]);
+
+  return (
+    <S.PostingContainer>
+      <S.PhotoGrid>
+        {isImg ? <S.Photo src={attachment_image} /> : <></>}
+      </S.PhotoGrid>
+      <S.Text>{content}</S.Text>
+    </S.PostingContainer>
+  );
 };
 
 export default InterpretationDetailPosting;
