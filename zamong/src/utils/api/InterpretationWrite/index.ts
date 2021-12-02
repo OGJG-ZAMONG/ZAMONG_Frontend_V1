66589414fd907f750092fc20c19c1bb237558a7c
김진근 <freedom7113@gmail.com>
2021-12-03 02:08:29 +0900

    ⚡️ :: (#371) add api connect

--- zamong/src/utils/api/InterpretationWrite/index.ts
+++ zamong/src/utils/api/InterpretationWrite/index.ts
@@ -1,10 +1,19 @@
 import uri from "../../../constance/uri";
 import { postInterpretationRequest } from "../../../models/dto/request/postInterpretationRequest";
+import { interpretationDetailResponse } from "../../../models/dto/response/interpretationDetailResponse";
 import instance from "../axios";
 
 export const postInterpretation = async (data: postInterpretationRequest) => {
   try {
     return await instance.post(uri.interpretation, data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const getInterpretationDetail = async (uuid: string) => {
+  try {
+    return await instance.get<interpretationDetailResponse>(uri.interpretation);
   } catch (error) {
     return Promise.reject(error);
   }
