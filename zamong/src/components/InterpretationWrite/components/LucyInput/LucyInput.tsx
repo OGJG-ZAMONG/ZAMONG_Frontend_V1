fabdc463ccdfd81b47e1f6103e464d2bf0e5d0b1
김진근 <freedom7113@gmail.com>
2021-12-03 01:08:18 +0900

    ⚡️ :: (#370) use props

--- zamong/src/components/InterpretationWrite/components/LucyInput/LucyInput.tsx
+++ zamong/src/components/InterpretationWrite/components/LucyInput/LucyInput.tsx
@@ -1,8 +1,13 @@
 import Selecter from "../../../DiaryWrite/component/Properties/Selecter/Selecter/Selecter";
 import Content from "./Content/Content";
 
-const LucyInput = (): JSX.Element => {
-  return <Selecter title="지급 LUCY" content={<Content />} />;
+interface PropsType {
+  lucy: number;
+  setLucy: (lucy: number) => void;
+}
+
+const LucyInput = ({ lucy, setLucy }: PropsType): JSX.Element => {
+  return <Selecter title="지급 LUCY" content={<Content lucy={lucy} setLucy={setLucy} />} />;
 };
 
 export default LucyInput;
