3ac864c5e1b840db86ba6069e6b0977fb81f6460
김진근 <freedom7113@gmail.com>
2021-12-03 01:01:34 +0900

    ⚡️ :: (#370) use selecter

--- zamong/src/components/InterpretationWrite/components/LucyInput/LucyInput.tsx
+++ zamong/src/components/InterpretationWrite/components/LucyInput/LucyInput.tsx
@@ -1,3 +1,8 @@
-const LucyInput = () => {};
+import Selecter from "../../../DiaryWrite/component/Properties/Selecter/Selecter/Selecter";
+import Content from "./Content/Content";
+
+const LucyInput = (): JSX.Element => {
+  return <Selecter title="지급 LUCY" content={<Content />} />;
+};
 
 export default LucyInput;
