4569162060f4bad44648baafd7768ae1043c977f
김진근 <freedom7113@gmail.com>
2021-12-02 18:24:51 +0900

    ⚡️ :: (#323) use skeleton

--- zamong/src/components/Sell/Main/SellMain.tsx
+++ zamong/src/components/Sell/Main/SellMain.tsx
@@ -3,13 +3,16 @@ import React, { FC, useState, useEffect } from "react";
 import SellingDream from "../../CardDream/SellingDream/SellingDream";
 import { getCurrentSellingDreams } from "../../../utils/api/Sell/Main";
 import PageNation from "../../PageNation/PageNation";
+import CardSkeleton from "../../CardSkeleton/CardSkeleton";
 
 const SellMain: FC = (): JSX.Element => {
   const [totalPage, setMaxPage] = useState<number>(0);
   const [pageIndex, setPageIndex] = useState<number>(0);
-  const [dreamData, setDreamData] = useState<Array<object>>([]);
+  const [dreamData, setDreamData] = useState<Array<object> | null>(null);
 
   const settingDreams = async () => {
+    setDreamData(null);
+
     try {
       const response = await getCurrentSellingDreams(pageIndex);
 
@@ -29,6 +32,27 @@ const SellMain: FC = (): JSX.Element => {
     });
   }, [pageIndex]);
 
+  const renderDream = dreamData?.map((value: any) => {
+    const { uuid, user, cost, updated_at, title, dream_types, default_posting_image } = value;
+    return (
+      <SellingDream
+        uuid={uuid}
+        user={user}
+        price={cost}
+        date={updated_at}
+        title={title}
+        tag={dream_types}
+        img={default_posting_image}
+      />
+    );
+  });
+
+  const renderSkeleton = Array<number>(16)
+    .fill(0)
+    .map((_, index) => {
+      return <CardSkeleton key={index} />;
+    });
+
   return (
     <S.Container>
       <S.Information>
@@ -39,20 +63,7 @@ const SellMain: FC = (): JSX.Element => {
       </S.Information>
       <S.SellingDreamListText>판매중인 꿈 목록</S.SellingDreamListText>
       <S.SellingDreamListContainer>
-        {dreamData.map((value: any) => {
-          const { uuid, user, cost, updated_at, title, dream_types, default_posting_image } = value;
-          return (
-            <SellingDream
-              uuid={uuid}
-              user={user}
-              price={cost}
-              date={updated_at}
-              title={title}
-              tag={dream_types}
-              img={default_posting_image}
-            />
-          );
-        })}
+        {dreamData ? renderDream : renderSkeleton}
       </S.SellingDreamListContainer>
       <PageNation max={totalPage} indexState={[pageIndex, setPageIndex]} columnCount={10} />
     </S.Container>
