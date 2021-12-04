223dd1b247e0b6aaef37a27cc3b7f35f07aaed5e
김진근 <freedom7113@gmail.com>
2021-12-04 14:11:40 +0900

    ⚡️ :: (#437) apply skeleton on following list

--- zamong/src/components/MainPage/FollowList/FollowList.tsx
+++ zamong/src/components/MainPage/FollowList/FollowList.tsx
@@ -6,7 +6,8 @@ import { following } from "../../../models/dto/response/followingsResponse";
 import { getFollowList } from "../../../utils/api/Main";
 
 const FollowList = (): JSX.Element => {
-  const [followings, setFollowings] = useState<following[]>([]);
+  const [followings, setFollowings] = useState<following[] | null>(null);
+  const nnFollowings = followings || []; //null이 아닌 배열
 
   const setFollowList = async () => {
     const param = {
@@ -22,8 +23,12 @@ const FollowList = (): JSX.Element => {
     }
   };
 
-  const followersRender = followings.slice(0, 8).map((value) => {
+  const followersRender = nnFollowings.slice(0, 8).map((value) => {
     return <S.Follow img={value.profile} to={`/user/${value.uuid}`} />;
+  });
+
+  const renderSkeleton = [1, 2, 3, 4].map((_, index) => {
+    return <S.FollowSkeleton key={index} />;
   });
 
   useLayoutEffect(() => {
@@ -32,13 +37,13 @@ const FollowList = (): JSX.Element => {
 
   return (
     <>
-      {followings.length > 0 && (
+      {(nnFollowings.length > 0 || !followings) && (
         <S.Container>
           <S.SectionTitle>
             <span>팔로잉</span>
           </S.SectionTitle>
           <S.FollowContainer>
-            {followersRender}
+            {followings ? followersRender : renderSkeleton}
             <S.MoreIcon alt="more" src={More} />
           </S.FollowContainer>
         </S.Container>
