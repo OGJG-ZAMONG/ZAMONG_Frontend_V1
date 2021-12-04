fa836a40c04b3cb0137f39e4c46a12b1c943d41c
kky04994 <kkyo8747@naver.com>
2021-12-05 06:40:45 +0900

    ⚡️ :: (#363) 비번 변경 연동 후 처리

--- zamong/src/components/Profile/Account/AccountContent.tsx
+++ zamong/src/components/Profile/Account/AccountContent.tsx
@@ -4,6 +4,7 @@ import {
   changeProfile,
   getMyProfile,
   modfiyId,
+  changePw,
 } from "../../../utils/api/Profile";
 import * as S from "./style";
 
@@ -17,6 +18,9 @@ const AccountContent = (): JSX.Element => {
     id: "",
   });
   const [newId, setNewId] = useState(idState.id);
+  const [isPwCheck, setPwCheck] = useState(true);
+  const [newPw, setNewPw] = useState("");
+  const [oldPw, setOldPw] = useState("");
 
   const myId = async () => {
     try {
@@ -39,12 +43,32 @@ const AccountContent = (): JSX.Element => {
     }
   };
 
-  const doChangeId = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>, newId: string) => {
+  const doChangeId = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    newId: string
+  ) => {
     if (newId === idState.id) {
       alert("현재 아이디와 같습니다.");
     } else {
       try {
         await modfiyId(newId);
+        window.location.reload();
+      } catch (error) {
+        throw error;
+      }
+    }
+  };
+
+  const doChangePW = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    oldPw: string,
+    newPw: string
+  ) => {
+    if (oldPw === newPw) {
+      alert("현비밀번호과 바꿀 비밀번호가 똑같습니다.");
+    } else {
+      try {
+        await changePw(oldPw, newPw);
         window.location.reload();
       } catch (error) {
         throw error;
@@ -66,6 +90,24 @@ const AccountContent = (): JSX.Element => {
     setNewId(e.target.value);
   };
 
+  const changeOldPw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOldPw(e.target.value);
+  };
+
+  const changeNewPw = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPw(e.target.value);
+  };
+
+  const changePwBtn = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (e) {
+      if (isPwCheck === false) {
+        setPwCheck(true);
+      } else if (isPwCheck === true) {
+        setPwCheck(false);
+      }
+    }
+  };
+
   const clickDelete = () => {
     // eslint-disable-next-line no-restricted-globals
     const input = confirm("탈퇴하시겠습니까?");
@@ -73,7 +115,9 @@ const AccountContent = (): JSX.Element => {
       const inputString = prompt("현재 비밀번호를 입력하세요", "비밀번호");
       alert(inputString);
       doDelete(inputString!);
-    } else return;
+    } else if (input === false) {
+      alert("취소가 되었습니다.");
+    }
   };
 
   const doDelete = async (pw: string) => {
@@ -101,7 +145,12 @@ const AccountContent = (): JSX.Element => {
             <S.SubTitle>프로필 사진 수정</S.SubTitle>
             <S.FileBox>
               <S.FileText htmlFor="profile">변경</S.FileText>
-              <S.FileBtn type="file" accept="image/*" onChange={upProfile} id="profile" />
+              <S.FileBtn
+                type="file"
+                accept="image/*"
+                onChange={upProfile}
+                id="profile"
+              />
             </S.FileBox>
           </S.Box>
         </S.ProfileBox>
@@ -109,26 +158,66 @@ const AccountContent = (): JSX.Element => {
           <S.TitleText>계정</S.TitleText>
           <S.Box>
             <S.SubTitle>아이디</S.SubTitle>
-            <S.ChangeContainer>
+            <S.IdContainer>
               {isIdCheck ? (
                 <S.IdText>{idState.id}</S.IdText>
               ) : (
-                <S.inputId onChange={(e) => changeId(e)} defaultValue={idState.id}></S.inputId>
+                <S.Input
+                  onChange={(e) => changeId(e)}
+                  defaultValue={idState.id}
+                />
               )}
               {isIdCheck ? null : (
-                <S.BorderButton onClick={(e) => doChangeId(e, newId)}>변경</S.BorderButton>
+                <S.BorderButton onClick={(e) => doChangeId(e, newId)}>
+                  변경
+                </S.BorderButton>
               )}
               {isIdCheck ? (
-                <S.BorderButton onClick={(e) => changeIdBtn(e)}>변경</S.BorderButton>
+                <S.BorderButton onClick={(e) => changeIdBtn(e)}>
+                  변경
+                </S.BorderButton>
               ) : (
-                <S.BorderButtonRed onClick={(e) => changeIdBtn(e)}>취소</S.BorderButtonRed>
+                <S.BorderButtonRed onClick={(e) => changeIdBtn(e)}>
+                  취소
+                </S.BorderButtonRed>
               )}
-            </S.ChangeContainer>
+            </S.IdContainer>
           </S.Box>
-          <S.Box>
+          <S.PwBox height={isPwCheck ? 180 : null}>
             <S.SubTitle>비밀번호</S.SubTitle>
-            <S.ButtonTo to="/findpassword">변경</S.ButtonTo>
-          </S.Box>
+            <S.PwContainer>
+              <div>
+                {isPwCheck ? null : (
+                  <S.BorderButton onClick={(e) => doChangePW(e, oldPw, newPw)}>
+                    변경
+                  </S.BorderButton>
+                )}
+                {isPwCheck ? (
+                  <S.BorderButton onClick={(e) => changePwBtn(e)}>
+                    변경
+                  </S.BorderButton>
+                ) : (
+                  <S.BorderButtonRed onClick={(e) => changePwBtn(e)}>
+                    취소
+                  </S.BorderButtonRed>
+                )}
+              </div>
+              {isPwCheck ? null : (
+                <>
+                  <S.InputContainer>
+                    <S.Input
+                      onChange={(e) => changeOldPw(e)}
+                      placeholder="현재 비밀번호를 입력해 주세요."
+                    />
+                    <S.Input
+                      onChange={(e) => changeNewPw(e)}
+                      placeholder="새로운 비밀번호를 입력해 주세요."
+                    />
+                  </S.InputContainer>
+                </>
+              )}
+            </S.PwContainer>
+          </S.PwBox>
           <S.Box>
             <S.DeleteText>계정 탈퇴</S.DeleteText>
             <S.BorderButtonRed onClick={clickDelete}>탈퇴</S.BorderButtonRed>
