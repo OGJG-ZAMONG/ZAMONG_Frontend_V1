c01373e38b6713136cd58b195709f8799431804b
kky04994 <kkyo8747@naver.com>
2021-12-05 16:29:23 +0900

    ⚡️ :: (#288) Dream 스타일 추가

--- zamong/src/components/Sell/SellManagement/DreamContent/DreamContent.tsx
+++ zamong/src/components/Sell/SellManagement/DreamContent/DreamContent.tsx
@@ -8,24 +8,50 @@ interface Props {
   date: string;
   tag: Array<string>;
   title: string;
+  defalut_image: string;
+  user: {
+    uuid: string;
+    profile: string;
+    id: string;
+  };
+  uuid: string;
 }
 
-const DreamContent: FC<Props> = ({ price, date, tag, title }): JSX.Element => {
+const DreamContent: FC<Props> = ({
+  price,
+  date,
+  tag,
+  title,
+  defalut_image,
+  user,
+  uuid,
+}): JSX.Element => {
+  const dateToString = (date: Date) => {
+    if (date.getFullYear() !== new Date().getFullYear()) {
+      return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    }
+
+    return `${date.getMonth() + 1}월 ${date.getDate()}일`;
+  };
   return (
     <>
       <S.ContentBox>
-        <S.TopBox img={DefaultImage}>
-          <S.Price>{price}₩</S.Price>
-          <S.Date>{date}</S.Date>
+        <S.TopBox img={defalut_image}>
+          <S.Price>{price}원</S.Price>
+          <S.Date>{dateToString(new Date(date))}</S.Date>
         </S.TopBox>
         <S.BottomBox>
+          <S.UserInfoContainer>
+            <S.ProfilePicture src={user.profile} />
+            <S.UserName>{user.id}</S.UserName>
+          </S.UserInfoContainer>
           <S.Tag>
             {tag.map((value: string, index: number) => {
               return <Tag key={index}>{value}</Tag>;
             })}
           </S.Tag>
-          <S.Title>{title}</S.Title>
         </S.BottomBox>
+        <S.Title>{title}</S.Title>
       </S.ContentBox>
     </>
   );
--- zamong/src/components/Sell/SellManagement/DreamContent/style.ts
+++ zamong/src/components/Sell/SellManagement/DreamContent/style.ts
@@ -21,38 +21,82 @@ export const TopBox = styled.div<{ img: string }>`
 `;
 
 export const Price = styled.div`
-  aspect-ratio: 2.3 / 1;
-  background: ${color.gray};
+  backdrop-filter: blur(10px);
+  background: rgb(0, 0, 0, 0.4);
   border-radius: 100px;
-  color: white;
-  font: ${font.body2};
+  color: ${color.white};
+  padding: 5px 8px;
+  font: ${font.description};
   display: flex;
   justify-content: center;
-  padding: 6px 8px;
+  align-items: center;
+  z-index: 1;
 `;
 
 export const Date = styled.div`
-  font: ${font.body2};
-  padding: 5px;
+  height: 24px;
+  font: ${font.description};
+  background-color: ${color.black};
+  backdrop-filter: blur(10px);
+  color: ${color.white};
+  background: rgb(0, 0, 0, 0.4);
+  padding: 3px 5px;
+  border-radius: 5px;
+  z-index: 1;
 `;
 
 export const BottomBox = styled.div`
-  width: 305px;
-  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  line-height: 36px;
+  height: 36px;
+  color: ${color.white};
+  margin: 12px 0;
+`;
+
+export const UserInfoContainer = styled.div`
+  display: flex;
+  width: 50%;
+`;
+
+export const ProfilePicture = styled.img`
+  max-width: 36px;
+  min-width: 36px;
+  min-height: 36px;
+  max-height: 36px;
+  border-radius: 100%;
+  margin-right: 6%;
+`;
+
+export const UserName = styled.div`
+  width: 100%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  word-break: keep-all;
+  overflow: hidden;
 `;
 
 export const Tag = styled.div`
+  width: 80%;
   display: flex;
-  justify-self: default;
-  margin-top: 12px;
+  justify-content: right;
+  align-items: center;
   div {
     margin: 0 1%;
   }
 `;
 
 export const Title = styled.div`
+  margin-top: 16px;
+  font: ${font.body3};
   color: ${color.white};
-  font: ${font.body2};
-  padding: 0 4px;
-  margin-top: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+  line-height: 1.5em;
+  max-height: 3em;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  word-break: break-all;
 `;
