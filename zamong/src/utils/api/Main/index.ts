5ef2087ada8de96292b6c3faf158d57b212fba58
김진근 <freedom7113@gmail.com>
2021-12-05 12:59:53 +0900

    ⚡️ :: (#435) change response

--- zamong/src/utils/api/Main/index.ts
+++ zamong/src/utils/api/Main/index.ts
@@ -1,16 +1,27 @@
 import uri from "../../../constance/uri";
 import { shareDreamRequest } from "../../../models/dto/request/shareDreamRequest";
 import { shareDreamWithSortRequest } from "../../../models/dto/request/shareDreamWithSortRequest";
-import { shareDreamResponse } from "../../../models/dto/response/shareDreamResponse";
+import { Dream, shareDreamResponse } from "../../../models/dto/response/shareDreamResponse";
 import { followingResponse } from "../../../models/dto/response/followingsResponse";
 import { getMyProfile } from "../Profile";
 import { dreamListResponse } from "../../../models/dto/response/dreamListResponse";
 import instance, { request } from "../axios";
 import { myDiaryRequest } from "../../../models/dto/request/myDiaryRequest";
+import defaultResponse from "../../../models/dto/response/defaultResponse";
+
+export interface followDream extends Dream {
+  user_uuid: string;
+}
+
+export type followDreamResponse = defaultResponse<{
+  share_dreams: followDream[];
+  total_page: number;
+  total_size: number;
+}>;
 
 export const getFollowShareDream = async (param: shareDreamRequest) => {
   try {
-    const response = await instance.get<shareDreamResponse>(uri.dreamShareFollow, {
+    const response = await instance.get<followDreamResponse>(uri.dreamShareFollow, {
       params: param,
     });
     return response;
