23646841c5a9a697a181ff58c5949b9a99dedd1d
김진근 <freedom7113@gmail.com>
2021-12-05 16:52:50 +0900

    ⚡️ :: (#288) change interface

--- zamong/src/models/dto/response/sellManagementResponse.ts
+++ zamong/src/models/dto/response/sellManagementResponse.ts
@@ -4,22 +4,22 @@ export interface myOnSaleManagement {
   content: {
     collection_value: boolean;
     response: {
-      sell_dreams: [
-        {
-          uuid: string;
-          default_posting_image: string;
-          title: string;
-          dream_types: Array<string>;
-          updated_at: string;
-          cost: number;
-          user: {
-            uuid: string;
-            id: string;
-            profile: string;
-          };
-        }
-      ];
+      sell_dreams: SellDream[];
     };
+  };
+}
+
+export interface SellDream {
+  uuid: string;
+  default_posting_image: string;
+  title: string;
+  dream_types: Array<string>;
+  updated_at: string;
+  cost: number;
+  user: {
+    uuid: string;
+    id: string;
+    profile: string;
   };
 }
 
