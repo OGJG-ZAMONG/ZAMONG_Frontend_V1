7156c17f742d6cd5ccf0ceaa3087b3e7ea82dc47
김진근 <freedom7113@gmail.com>
2021-12-05 18:15:51 +0900

    ⚡️ :: (#288) pagenation

--- zamong/src/components/Sell/SellManagement/SellManagement.tsx
+++ zamong/src/components/Sell/SellManagement/SellManagement.tsx
@@ -1,5 +1,5 @@
 import * as S from "./style";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import {
   getSellMyOnSaleManagement,
   getSellSoldOutManagement,
@@ -11,16 +11,32 @@ import MySellDreamCard from "../../CardDream/MySellDreamCard/MySellDreamCard";
 const SellManagement: FC = (): JSX.Element => {
   const [myOnSaleData, setMyOnSaleData] = useState<Array<object> | null>(null);
   const [mySoldOutData, setMySoldOutData] = useState<Array<object> | null>(null);
-  const [page, setMaxPage] = useState<number>(0);
-  const [pageIndex, setPageIndex] = useState<number>(0);
+  const [salePage, setSalePage] = useState<number>(0);
+  const [soldOutPage, setSoldOutPage] = useState<number>(0);
+  const [saleMax, setSaleMax] = useState<number>(0);
+  const [soldOutMax, setSoldOutMax] = useState<number>(0);
+  const saleContainerRef = useRef<HTMLDivElement>(null);
+  const soldContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     myOnSaleManagement();
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  }, [pageIndex]);
+    if (saleContainerRef.current) {
+      window.scrollTo({
+        top: saleContainerRef.current.clientTop - 64,
+        behavior: "smooth",
+      });
+    }
+  }, [salePage]);
+
+  useEffect(() => {
+    mySoldOutManagement();
+    if (soldContainerRef.current) {
+      window.scrollTo({
+        top: soldContainerRef.current.clientTop - 64,
+        behavior: "smooth",
+      });
+    }
+  }, [soldOutPage]);
 
   const renderSkeleton = Array<number>(8)
     .fill(0)
@@ -30,9 +46,10 @@ const SellManagement: FC = (): JSX.Element => {
 
   const myOnSaleManagement = async () => {
     try {
-      const response = await getSellMyOnSaleManagement(pageIndex);
-      setMyOnSaleData(response.data.content.response.sell_dreams);
-      console.log(response.data.content.response);
+      const response = await getSellMyOnSaleManagement(salePage);
+      const { total_page, sell_dreams } = response.data.content.response;
+      setSaleMax(total_page);
+      setMyOnSaleData(sell_dreams);
     } catch (error) {
       throw error;
     }
@@ -41,18 +58,14 @@ const SellManagement: FC = (): JSX.Element => {
   const mySoldOutManagement = async () => {
     setMyOnSaleData(null);
     try {
-      const response = await getSellSoldOutManagement(pageIndex);
-      setMySoldOutData(response.data.content.response.sell_dreams);
-      console.log(response.data.content);
+      const response = await getSellSoldOutManagement(soldOutPage);
+      const { total_page, sell_dreams } = response.data.content.response;
+      setSoldOutMax(total_page);
+      setMySoldOutData(sell_dreams);
     } catch (error) {
       throw error;
     }
   };
-
-  useEffect(() => {
-    myOnSaleManagement();
-    mySoldOutManagement();
-  }, []);
 
   const renderOnSaleDream = myOnSaleData?.map((value: any, index) => {
     return <MySellDreamCard key={index} data={value} />;
@@ -64,16 +77,14 @@ const SellManagement: FC = (): JSX.Element => {
 
   return (
     <S.Contents>
-      <S.SellManagementText>꿈 판매 관리</S.SellManagementText>
+      <S.SellManagementText ref={saleContainerRef}>꿈 판매 관리</S.SellManagementText>
       <S.Box>
         <S.SubTitle>나의 판매중인 꿈 목록</S.SubTitle>
         <S.List>{myOnSaleData ? renderOnSaleDream : renderSkeleton}</S.List>
-        {pageIndex >= 1 ? (
-          <PageNation max={page} indexState={[pageIndex, setPageIndex]} columnCount={10} />
-        ) : null}
+        <PageNation max={saleMax} indexState={[salePage, setSalePage]} columnCount={10} />
       </S.Box>
       <S.Box>
-        <S.SubTitle>나의 판매한 꿈 목록</S.SubTitle>
+        <S.SubTitle ref={soldContainerRef}>나의 판매한 꿈 목록</S.SubTitle>
         <S.List>
           {mySoldOutData === null ? (
             <S.Text>공유된 꿈이 없습니다.</S.Text>
@@ -83,9 +94,7 @@ const SellManagement: FC = (): JSX.Element => {
             renderSkeleton
           )}
         </S.List>
-        {pageIndex >= 1 ? (
-          <PageNation max={page} indexState={[pageIndex, setPageIndex]} columnCount={10} />
-        ) : null}
+        <PageNation max={soldOutMax} indexState={[soldOutPage, setSoldOutPage]} columnCount={10} />
       </S.Box>
     </S.Contents>
   );
