713cdf8230772cfd3638812d4454765d0cc57647
김진근 <freedom7113@gmail.com>
2021-12-08 11:45:59 +0900

    ⚡️ :: (#499) add object

--- zamong/src/components/Profile/Badge/Badge.tsx
+++ zamong/src/components/Profile/Badge/Badge.tsx
@@ -1,4 +1,9 @@
 import { useLayoutEffect, useState } from "react";
+import Bronze from "../../../assets/Badge/Bronze.svg";
+import Silver from "../../../assets/Badge/Silver.svg";
+import Gold from "../../../assets/Badge/Gold.svg";
+import Diamond from "../../../assets/Badge/Diamond.svg";
+import Master from "../../../assets/Badge/Master.svg";
 
 interface PropsType {
   count: number;
@@ -8,10 +13,16 @@ const Badge = ({ count }: PropsType) => {
   const BRONZE = "BRONZE";
   const SILVER = "SILVER";
   const GOLD = "GOLD";
-  const BLUE = "BLUE";
-  const RED = "RED";
+  const DIAMOND = "DIAMOND";
+  const MASTER = "MASTER";
 
-    
+  const images = {
+    BRONZE: Bronze,
+    SILVER: Silver,
+    GOLD: Gold,
+    DIAMOND: Diamond,
+    MASTER: Master,
+  };
 
   const [state, setState] = useState<string | null>(null);
 
